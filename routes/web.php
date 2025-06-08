71d6ce75c7549cb834368d0a4c8b72c922ecef1b
Suban <subandrio140705@gmail.com>
2025-06-09 01:43:26 +0700

    Customers Auth Added

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CustomerAuthController;
 
 Route::get('/', [HomepageController::class, 'index'])->name('home');
 Route::get('products', [HomepageController::class, 'products']);
@@ -20,6 +21,29 @@ Route::group(['prefix'=>'dashboard','middleware'=>['auth','verified']], function
 
     Route::resource('categories',ProductCategoryController::class);
     Route::resource('products',ProductController::class);
+});
+
+Route::group(['prefix'=>'customer'], function(){
+    Route::controller(CustomerAuthController::class)->group(function(){
+        Route::group(['middleware'=>'check_customer_login'], function(){
+            //tampilkan halaman login
+            Route::get('login','login')->name('customer.login');
+
+            //aksi login
+            Route::post('login','store_login')->name('customer.store_login');
+
+            //tampilkan halaman register
+            Route::get('register','register')->name('customer.register');
+
+            //aksi register
+            Route::post('register','store_register')->name('customer.store_register');
+        });
+        
+
+        //aksi logout
+        Route::post('logout','logout')->name('customer.logout');
+
+    });
 });
 
 
