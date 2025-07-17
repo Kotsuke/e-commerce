03a10056e6d58a88e7cfc95e81654869c35dd084
JustParadis <panjirafi96@gmail.com>
2025-07-17 14:59:55 +0700

    logging v2

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use App\Models\Categories;
+use Illuminate\Support\Facades\Log;
+
 class ProductCategoryController extends Controller
 {
     /**
