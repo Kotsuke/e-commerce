e46f9d46d2a64e39462cf20fdbee39e0b623ae59
JustParadis <panjirafi96@gmail.com>
2025-07-17 15:51:03 +0700

    footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,49 @@
+<footer class="bg-dark text-white pt-4 mt-5" style="background: black;">
+    <div class="container p-3">
+        <div class="row">
+            <div class="col-md-4 mb-4 p-3">
+                <h5 class="mb-3">KUEQ</h5>
+                <p class="small">Belanja mudah, cepat, dan aman di toko
+                    online kami. Temukan produk favorit Anda dengan harga terbaik.</p>
+                <div class="social-icons mt-3">
+                    <a href="#" class="text-white me-3"><i class="bi bi-facebook fs-5"></i></a>
+                    <a href="#" class="text-white me-3"><i class="bi bi-instagram fs-5"></i></a>
+                    <a href="#" class="text-white me-3"><i class="bi bi-twitter fs-5"></i></a>
+                    <a href="#" class="text-white"><i class="bi bi-linkedin fs-5"></i></a>
+                </div>
+            </div>
+
+            <div class="col-md-2 mb-4 p-3"> <h6 class="mb-3">Navigasi</h6>
+                <ul class="list-unstyled">
+                    <li><a href="#" class="text-white text-decoration-none">Beranda</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Produk</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Kategori</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Tentang Kami</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Blog</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 mb-4 p-3"> <h6 class="mb-3">Bantuan & Informasi</h6>
+                <ul class="list-unstyled small">
+                    <li><a href="#" class="text-white text-decoration-none">FAQ</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Kebijakan Privasi</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Syarat & Ketentuan</a></li>
+                    <li><a href="#" class="text-white text-decoration-none">Pengembalian Produk</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-3 mb-4 p-3"> <h6 class="mb-3">Kontak Kami</h6>
+                <ul class="list-unstyled small">
+                    <li><a href="mailto:kueq@gmail.com" class="text-white text-decoration-none"><i class="bi bi-envelope me-2"></i> kueq@gmail.com</a></li>
+                    <li><a href="tel:+628566100994" class="text-white text-decoration-none"><i class="bi bi-telephone me-2"></i> +62 856 6100 994</a></li>
+                    <li><i class="bi bi-geo-alt me-2"></i> Tegal, Indonesia</li>
+                    <li><a href="#" class="text-white text-decoration-none"><i class="bi bi-whatsapp me-2"></i> WhatsApp</a></li>
+                </ul>
+                </div>
+        </div>
+        <hr class="bg-secondary my-4">
+        <div class="text-center pb-3">
+            <small>&copy; {{ date('Y') }} KUEQ. All rights reserved.</small>
+        </div>
+    </div>
+</footer>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,15 +7,18 @@
    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
    <title>{{ env('APP_NAME') }} {{ $title ?? ''}}</title>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-  
+
    <x-navbar></x-navbar>
 
    <div class="container-fluid py-4">
        {{  $slot }}
    </div>
-  
+
+    <x-footer></x-footer>
+
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
